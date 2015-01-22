5242d6678ebcd185d804984a96d3bacabb3ea99b
nnrobot1 <liferoxinc@gmail.com>
2015-01-21 18:39:33 -0600

    initial javascrit work

--- scouting/scouting.js
+++ scouting/scouting.js
@@ -1,3 +1,7 @@
+tele = new Meteor.Collection("teleop");
+auto = new Meteor.Collection("autonomous");
+pit = new Meteor.Collection("pitScout");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
@@ -12,6 +16,37 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
+    }
+  });
+
+  Template.add_item.events({
+    'click .submit': function(){
+      $tele.insert({
+        minipulateRecyclingBins: $('.recycling bins').val(),
+        minipulateTotes: $('.totes').val(),
+        minipulateNoodles: $('.noodles').val(),
+        coopertitionStack: $('.stack').val(),
+        coopertitionSet: $('.set').val(),
+        collectsTotesFromHuman: $('.human').val(),
+        collectsTotesFromGround: $('.ground').val(),
+        totePreference: $('.totePreference').val(),
+        tipTotes: $('.tipObjects').val(),
+        numberStakesKnokedDown: $('.numberStakesKnokedDown').val(),
+        maxCrates: $('.maxCrates').val(),
+        goOverPlatform: $('.goOverPlatform').val()
+
+      });
+    $auto.insert({
+        startingConfiguration: $('.startingConfig').val(),
+        totalBinsInAutoZone: $('.binsAuto').val(),
+        totalTotesInAutoZone: $('.totesAuto').val(),
+        totalNoodlesInAutoZone: $('.noodlesAuto').val(),
+        stackYellowBins: $('.stackedYellowBins').val(),
+        endInAutoZone: $('.endAutoZone').val()
+      });
+    $pit.insert({
+
+    })
     }
   });
 }
