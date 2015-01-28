99f31160c621f1d13b2ba85f4c604310c9004d56
nnrobot <nnrobot>
2015-01-28 16:18:50 -0600

    added working data storage

--- scouting/scouting.js
+++ scouting/scouting.js
@@ -1,54 +1,53 @@
-tele = new Meteor.Collection("teleop");
-auto = new Meteor.Collection("autonomous");
-pit = new Meteor.Collection("pitScout");
+data = new Meteor.Collection("data");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-
-  Template.add_item.events({
-    'click .submit': function(){
-      $tele.insert({
-        minipulateRecyclingBins: $('.recycling bins').val(),
-        minipulateTotes: $('.totes').val(),
-        minipulateNoodles: $('.noodles').val(),
-        coopertitionStack: $('.stack').val(),
-        coopertitionSet: $('.set').val(),
-        collectsTotesFromHuman: $('.human').val(),
-        collectsTotesFromGround: $('.ground').val(),
-        totePreference: $('.totePreference').val(),
-        tipTotes: $('.tipObjects').val(),
-        numberStakesKnokedDown: $('.numberStakesKnokedDown').val(),
-        maxCrates: $('.maxCrates').val(),
-        goOverPlatform: $('.goOverPlatform').val()
-
+  Template.form.events({
+    'click button': function() {
+      var input = document.getElementById("input").value;
+      alert(input);
+      data.insert({
+        'input':input
       });
-    $auto.insert({
-        startingConfiguration: $('.startingConfig').val(),
-        totalBinsInAutoZone: $('.binsAuto').val(),
-        totalTotesInAutoZone: $('.totesAuto').val(),
-        totalNoodlesInAutoZone: $('.noodlesAuto').val(),
-        stackYellowBins: $('.stackedYellowBins').val(),
-        endInAutoZone: $('.endAutoZone').val()
-      });
-    $pit.insert({
-
-    })
     }
   });
+
+  Template.get_data.events({
+    'click button': function() {
+      alert(JSON.stringify(data.find()));
+    }
+  });
+
+  // Template.add_item.events({
+  //   'click .submit': function(){
+  //     $tele.insert({
+  //       minipulateRecyclingBins: $('.recycling bins').val(),
+  //       minipulateTotes: $('.totes').val(),
+  //       minipulateNoodles: $('.noodles').val(),
+  //       coopertitionStack: $('.stack').val(),
+  //       coopertitionSet: $('.set').val(),
+  //       collectsTotesFromHuman: $('.human').val(),
+  //       collectsTotesFromGround: $('.ground').val(),
+  //       totePreference: $('.totePreference').val(),
+  //       tipTotes: $('.tipObjects').val(),
+  //       numberStakesKnokedDown: $('.numberStakesKnokedDown').val(),
+  //       maxCrates: $('.maxCrates').val(),
+  //       goOverPlatform: $('.goOverPlatform').val()
+
+  //     });
+  //   $auto.insert({
+  //       startingConfiguration: $('.startingConfig').val(),
+  //       totalBinsInAutoZone: $('.binsAuto').val(),
+  //       totalTotesInAutoZone: $('.totesAuto').val(),
+  //       totalNoodlesInAutoZone: $('.noodlesAuto').val(),
+  //       stackYellowBins: $('.stackedYellowBins').val(),
+  //       endInAutoZone: $('.endAutoZone').val()
+  //     });
+  //   $pit.insert({
+
+  //   })
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
