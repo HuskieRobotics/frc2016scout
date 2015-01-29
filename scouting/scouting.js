d6143fb1c2fe11ba465c3adf88d62df9ba8d55e1
nnrobot <nnrobot>
2015-01-28 18:21:48 -0600

    finished html and js for teleop
    finish js for auto

--- scouting/scouting.js
+++ scouting/scouting.js
@@ -18,6 +18,35 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.match.events({
+    'click button': function() {
+      var rBin = document.getElementById("recycling").value;
+      var totes = document.getElementById("totes").value;
+      var noodles = document.getElementById("noodles").value;
+      var coopMode = document.getElementById("coopertitionOption").value;
+      var cGround = document.getElementById("fromArena").value;
+      var cHuman = document.getElementById("fromPlayer").value;
+      var colPref = document.getElementById("collectPreference").value;
+      var Tip = document.getElementById("tipTotes").value;
+      var sKnocked = document.getElementById("stacksKnocked").value;
+      var mCrates = document.getElementById("maxCrates").value;
+      var Platform = document.getElementById("overPlatform").value;
+      
+      //
+      //data.insert({
+      //  'manipulateRecycleBins':rBin
+      //})
+      //
+    }
+    
+  });
+
+  Template.get_data.events({
+    'click button': function() {
+      alert(JSON.stringify(data.find()));
+    }
+  });
+
   // Template.add_item.events({
   //   'click .submit': function(){
   //     $tele.insert({
