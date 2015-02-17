ef7f9a1283831d76909db095fcb2ea6406ee4dfa
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-02-17 16:00:50 -0600

    some sql work

--- scout/routes/index.js
+++ scout/routes/index.js
@@ -5,6 +5,26 @@ var mysql = require('mysql');
 var sql = require('sql');
 var jsonSql = require('json-sql')();
 
+
+// loads bauer-sql module
+var sql = require("bauer-sql");
+
+// builds data statements
+var select = sql.select();
+var insert = sql.insert();
+var update = sql.update();
+var delete = sql.delete();
+
+// builds structure statements
+var create = sql.create();
+var drop = sql.drop();
+var alter = sql.alter();
+
+// other utilities
+var parser = sql.parser();
+var schema = sql.schema();
+
+
 var columns = ['config',
   'bins_auto',
   'totes_auto',
