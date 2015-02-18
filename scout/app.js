ce1e90ea6a450967effa6fef4ac718d4447c4545
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-02-17 18:53:38 -0600

    SQL SAVING WORKS

--- scout/app.js
+++ scout/app.js
@@ -12,62 +12,66 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-// app.set('port', process.env.PORT || 3000);
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
-// app.use('/users', users);
+app.use('/', routes);
+app.use('/users', users);
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
 // production error handler
 // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-// });
-
-var server = app.listen(3000, function() {
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log('Scouting app listening at http://%s:%s', host, port)
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+app.get('/', function(req, res, next){
+  res.render('index', { title: 'Scouting' });
+});
+
+// var server = app.listen(3000, function() {
+//     var host = server.address().address
+//     var port = server.address().port
+
+//     console.log('Scouting app listening at http://%s:%s', host, port)
+// });
 
 // module.exports = app;
--- scout/routes/index.js
+++ scout/routes/index.js
@@ -2,90 +2,80 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql');
-var sql = require('sql');
+// // var sql = require('sql');
 var jsonSql = require('json-sql')();
 
-
-// loads bauer-sql module
+// // loads bauer-sql module
 var sql = require("bauer-sql");
 
 // builds data statements
-var select = sql.select();
 var insert = sql.insert();
-var update = sql.update();
-var delete = sql.delete();
 
-// builds structure statements
-var create = sql.create();
-var drop = sql.drop();
-var alter = sql.alter();
+// var columns = ['config',
+//   'bins_auto',
+//   'totes_auto',
+//   'noodles_auto',
+//   'bins',
+//   'auto',
+//   'recyclingBins',
+//   'totes',
+//   'noodles',
+//   'coop',
+//   'cGround',
+//   'cHuman',
+//   'collect',
+//   'pickup',
+//   'stacksKnocked',
+//   'maxCrates',
+//   'platform'
+//   ];
 
-// other utilities
-var parser = sql.parser();
-var schema = sql.schema();
+/**
+CREATE TABLE scouting
+(
+config TEXT NOT NULL,
+bins_auto TEXT NOT NULL,
+totes_auto TEXT NOT NULL,
+noodles_auto TEXT NOT NULL,
+bins TEXT NOT NULL,
+auto TEXT NOT NULL,
+recyclingBins TEXT NOT NULL,
+totes TEXT NOT NULL,
+noodles TEXT NOT NULL,
+coop TEXT NOT NULL,
+cGround TEXT NOT NULL,
+cHuman TEXT NOT NULL,
+collect TEXT NOT NULL,
+pickup TEXT NOT NULL,
+stacksKnocked TEXT NOT NULL,
+maxCrates TEXT NOT NULL,
+platform TEXT NOT NULL
+)
+*/
 
-
-var columns = ['config',
-  'bins_auto',
-  'totes_auto',
-  'noodles_auto',
-  'bins',
-  'auto',
-  'recylingBings',
-  'totes',
-  'noodles',
-  'coop',
-  'cGround',
-  'cHuman',
-  'collect',
-  'pickup',
-  'stacksKnocked',
-  'maxCrates',
-  'platform'
-  ];
-
-var table = sql.define({
-  name: 'table',
-  columns: [
-  'config',
-  'bins_auto',
-  'totes_auto',
-  'noodles_auto',
-  'bins',
-  'auto',
-  'recylingBings',
-  'totes',
-  'noodles',
-  'coop',
-  'cGround',
-  'cHuman',
-  'collect',
-  'pickup',
-  'stacksKnocked',
-  'maxCrates',
-  'platform'
-  ]
-});
 
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : ''
+  password : '',
+  database : 'scout'
 });
 
-connection.connect();
+// connection.connect();
 
-var createTable = 'CREATE TABLE $match( ';
+// var createTable = 'CREATE TABLE scouting( ';
 
-columns.forEach(function(e){
-  createTable += e + ' TEXT NOT NULL';
-});
+// columns.forEach(function(e){
+//   createTable += e + ' TEXT NOT NULL, ';
+// });
 
-createTable += ');';
+// createTable += ');';
 
-connection.query(createTable, function(err, rows, fields){
-  if (err) throw err;
-});
+// var createDB = 'CREATE DATABASE scout';
+
+// connection.query(, function(err, rows, fields){
+//   if (err) throw err;
+// });
 
 
 // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
@@ -105,7 +95,28 @@ router.post('/save', function(req, res, next) {
 	console.log(req.body);
 	// connection.query('', function(err, rows, fields) {
 	// 	console.log("oh");
-	// });
+	// // });s
+  
+
+  var query = "INSERT INTO scouting";
+  query += " (config, bins_auto, totes_auto, noodles_auto, bins, auto, recyclingBins, totes, noodles,";
+  query += " coop, cGround, cHuman, collect, pickup, stacksKnocked, maxCrates, platform) VALUES ('";
+  query += req.body.config + "', '" + req.body.bins_auto + "', '" + req.body.totes_auto + "', '" + req.body.noodles_auto + "', '";
+  query += req.body.bins + "', '" + req.body.auto + "', '" + req.body.recylingBins + "', '" + req.body.totes + "', '";
+  query += req.body.noodles + "', '" + req.body.coop + "', '" + req.body.cGround + "', '" + req.body.cHuman + "', '";
+  query += req.body.collect + "', '" + req.body.pickup + "', '" + req.body.stacksKnocked + "', '" + req.body.maxCrates + "', '";
+  query += req.body.platform + "');";
+
+  connection.connect();
+
+  console.log(query);
+
+  connection.query(query, function(err, rows, fields){
+    if (err) console.log(err);
+  });
+
+  connection.end();
+
 });
 
 // connection.end();
